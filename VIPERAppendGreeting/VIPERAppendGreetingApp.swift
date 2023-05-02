7dcd61b85ac4567f3fad1a525dfa68c5fd661461
Pac12Run8000 <gangstarr78992@>
2023-05-02 09:31:42 -0700

    Initial Commit

---
+++ VIPERAppendGreeting/VIPERAppendGreetingApp.swift
@@ -0,0 +1,17 @@
+//
+//  VIPERAppendGreetingApp.swift
+//  VIPERAppendGreeting
+//
+//  Created by Michelle Grover on 5/2/23.
+//
+
+import SwiftUI
+
+@main
+struct VIPERAppendGreetingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
