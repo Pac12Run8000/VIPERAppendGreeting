467e63c1ce4bc041c2561dcd7ac77b9b2b14a3bb
Pac12Run8000 <gangstarr78992@>
2023-05-02 10:55:35 -0700

    The first iteration with a functioning UI

---
+++ VIPERAppendGreeting/GreetingView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  VIPERAppendGreeting
+//
+//  Created by Michelle Grover on 5/2/23.
+//
+
+import SwiftUI
+
+struct GreetingView: View {
+    @State private var name = ""
+    @State private var greeting = ""
+
+    var body: some View {
+        VStack {
+            TextField("Name", text: $name)
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button("Generate Greeting") {
+                greeting = "Hello, \(name)!"
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.blue)
+            .cornerRadius(8)
+
+            Text(greeting)
+                .padding()
+                .frame(width: 200, height: 50)
+                .border(Color.gray, width: 1)
+            
+        }
+    }
+}
+
+struct GreetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        GreetingView()
+    }
+}
